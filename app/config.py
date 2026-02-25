4672dd4f92f1a593f7916340ce98bb6a93630ab8
Maximilian Rödiger <maxroediger@googlemail.com>
2026-02-25 12:56:32 +0100

    Add Amadeus configuration variables

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent  # project root
 INSTANCE_DIR = BASE_DIR / "instance"
@@ -8,3 +9,6 @@ class Config:
     INSTANCE_DIR.mkdir(exist_ok=True)
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{(INSTANCE_DIR / 'app.sqlite').as_posix()}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AMADEUS_BASE_URL = os.getenv("AMADEUS_BASE_URL", "https://test.api.amadeus.com")
+    AMADEUS_CLIENT_ID = os.getenv("AMADEUS_CLIENT_ID")
+    AMADEUS_CLIENT_SECRET = os.getenv("AMADEUS_CLIENT_SECRET")
